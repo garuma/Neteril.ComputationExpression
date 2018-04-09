c16827ab53d1843f4f606da0b99eb2c7c9626273
Jérémie Laval <jeremie.laval@gmail.com>
2018-04-08 20:47:41 -0400

    Add a proxy class with shorter "operator" method names that can be statically imported

---
+++ Neteril.ComputationExpression/Operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Neteril.ComputationExpression
+{
+	/// <summary>
+	/// Use this class with a `using static` statement to have a shorter
+	/// set of "operator" to work with instead of using the full length
+	/// methods in <see cref="ComputationExpression"/>
+	/// </summary>
+	public static class Operators
+	{
+		public static TMonad CxRun<T, TMonad> (IMonadExpressionBuilder builder, Func<IMonad<T>> body)
+			where TMonad : IMonad<T>
+		{
+			return ComputationExpression.Run<T, TMonad> (builder, body);
+		}
+
+		public static CombineAwaitable<T> CxYield<T> (T value) => ComputationExpression.Yield<T> (value);
+	}
+}
