386b849ce6601bc64d3c1bbbc1031bee67bf0748
Jérémie Laval <jeremie.laval@gmail.com>
2018-04-08 20:46:35 -0400

    Add unit-tests project

---
+++ Neteril.ComputationExpression.Tests/EnumerableTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+using static Neteril.ComputationExpression.Operators;
+using Neteril.ComputationExpression.Instances;
+
+namespace Neteril.ComputationExpression.Tests
+{
+	[TestFixture]
+	public class EnumerableTest
+	{
+		[Test]
+		public void NestedYield ()
+		{
+			var result = CxRun<int, EnumerableMonad<int>> (EnumerableExpressionBuilder.Instance, async () => {
+				var item = await (EnumerableMonad<int>)new[] { 1, 2, 3 };
+				var item2 = await (EnumerableMonad<int>)new[] { 100, 200 };
+				await CxYield (item);
+				await CxYield (item2);
+				return item * item2;
+			});
+
+			var expected = new[] {
+				1, 100, 100,
+				1, 200, 200,
+				2, 100, 200,
+				2, 200, 400,
+				3, 100, 300,
+				3, 200, 600
+			};
+
+			CollectionAssert.AreEqual (expected, result);
+		}
+	}
+}
---
+++ Neteril.ComputationExpression.Tests/MaybeTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+
+using static Neteril.ComputationExpression.Operators;
+using Neteril.ComputationExpression.Instances;
+
+namespace Neteril.ComputationExpression.Tests
+{
+	[TestFixture]
+	public class MaybeTest
+	{
+		[Test]
+		public void Good ()
+		{
+			var good = CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
+				var val1 = await TryDivide (120, 2);
+				var val2 = await TryDivide (val1, 3);
+				var val3 = await TryDivide (val2, 2);
+
+				return val3;
+			});
+
+			Assert.IsInstanceOf<Some<int>> (good);
+			Assert.AreEqual (10, ((Some<int>)good).Item);
+		}
+
+		[Test]
+		public void Bad ()
+		{
+			var bad = CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
+				var val1 = await TryDivide (120, 2);
+				var val2 = await TryDivide (val1, 0);
+				var val3 = await TryDivide (val2, 2);
+
+				return val3;
+			});
+
+			Assert.IsInstanceOf<None<int>> (bad);
+		}
+
+		static Option<int> TryDivide (int up, int down)
+		{
+			if (down == 0)
+				return None<int>.Value;
+			return Some.Of (up / down);
+		}
+	}
+}
