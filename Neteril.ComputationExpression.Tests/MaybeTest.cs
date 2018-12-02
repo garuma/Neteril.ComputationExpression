50cb82646b373ae16761ddd2bb909490d9ec117b
Jérémie Laval <jeremie.laval@gmail.com>
2018-12-01 19:46:38 -0500

    Update tests

--- Neteril.ComputationExpression.Tests/MaybeTest.cs
+++ Neteril.ComputationExpression.Tests/MaybeTest.cs
@@ -12,9 +12,11 @@ namespace Neteril.ComputationExpression.Tests
 		[Test]
 		public void Good ()
 		{
+			var called = false;
 			var good = CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
 				var val1 = await TryDivide (120, 2);
 				var val2 = await TryDivide (val1, 3);
+				called = true;
 				var val3 = await TryDivide (val2, 2);
 
 				return val3;
@@ -22,20 +24,24 @@ namespace Neteril.ComputationExpression.Tests
 
 			Assert.IsInstanceOf<Some<int>> (good);
 			Assert.AreEqual (10, ((Some<int>)good).Item);
+			Assert.IsTrue (called);
 		}
 
 		[Test]
 		public void Bad ()
 		{
+			var called = false;
 			var bad = CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
 				var val1 = await TryDivide (120, 2);
 				var val2 = await TryDivide (val1, 0);
+				called = true;
 				var val3 = await TryDivide (val2, 2);
 
 				return val3;
 			});
 
 			Assert.IsInstanceOf<None<int>> (bad);
+			Assert.IsFalse (called);
 		}
 
 		static Option<int> TryDivide (int up, int down)
@@ -43,6 +49,20 @@ namespace Neteril.ComputationExpression.Tests
 			if (down == 0)
 				return None<int>.Value;
 			return Some.Of (up / down);
+		}
+
+		[Test]
+		public void Nested ()
+		{
+			var outer = CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
+				var inner = await CxRun<int, Option<int>> (OptionExpressionBuilder.Instance, async () => {
+					var i = await new Some<int> (42);
+					return i;
+				});
+				return inner;
+			});
+			Assert.IsInstanceOf<Some<int>> (outer);
+			Assert.AreEqual (42, ((Some<int>)outer).Item);
 		}
 	}
 }
