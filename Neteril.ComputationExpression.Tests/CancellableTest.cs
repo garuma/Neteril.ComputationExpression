94693738d104999b99bd4fa9a043b4533476a6f3
Jérémie Laval <jeremie.laval@gmail.com>
2018-12-01 19:54:00 -0500

    Add cancellation monad

---
+++ Neteril.ComputationExpression.Tests/CancellableTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+using Neteril.ComputationExpression.Instances;
+using static Neteril.ComputationExpression.Operators;
+using static Neteril.ComputationExpression.Instances.Cancellable;
+
+namespace Neteril.ComputationExpression.Tests
+{
+	[TestFixture]
+	public class CancellableTests
+	{
+		[Test]
+		public void ReturnSimpleValue ()
+		{
+			var (result, wasCanceled) = CxRun<int, CancellableMonad<int>> (CancellableExpressionBuilder.Instance, async () => {
+				return await CancellableMonad<int>.FromValue (3);
+			}).Run (CancellationToken.None);
+			Assert.IsFalse (wasCanceled);
+			Assert.AreEqual (3, result);
+		}
+
+		[Test]
+		public void ReturnAfterOperations ()
+		{
+			var (result, wasCanceled) = CxRun<int, CancellableMonad<int>> (CancellableExpressionBuilder.Instance, async () => {
+				var op = await CancellableMonad<int>.FromValue (1);
+				op = await MaybeDo (() => op + 1);
+				op = await MaybeDo (() => op + 10);
+				return op;
+			}).Run (CancellationToken.None);
+			Assert.IsFalse (wasCanceled);
+			Assert.AreEqual (12, result);
+		}
+
+		[Test]
+		public void Cancelled_BeforeUse ()
+		{
+			var source = new CancellationTokenSource ();
+			source.Cancel ();
+			var (result, wasCanceled) = CxRun<int, CancellableMonad<int>> (CancellableExpressionBuilder.Instance, async () => {
+				var r = await CancellableMonad<int>.FromValue (42);
+				return r;
+			}).Run (source.Token);
+			Assert.IsTrue (wasCanceled);
+		}
+
+		[Test]
+		public void Cancelled_Between ()
+		{
+			var source = new CancellationTokenSource ();
+			var (result, wasCanceled) = CxRun<int, CancellableMonad<int>> (CancellableExpressionBuilder.Instance, async () => {
+				var op = await CancellableMonad<int>.FromValue (1);
+				op = await MaybeDo (() => op + 1);
+				source.Cancel ();
+				op = await MaybeDo (() => op + 10);
+				return op;
+			}).Run (source.Token);
+			Assert.IsTrue (wasCanceled);
+			Assert.AreEqual (2, result);
+		}
+	}
+}
---
+++ Neteril.ComputationExpression/Instances/CancellableMonad.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Neteril.ComputationExpression.Instances
+{
+	public class CancellableMonad<T> : IMonad<T>
+	{
+		Func<CancellationToken, (T, bool)> innerFunction;
+
+		internal CancellableMonad (Func<CancellationToken, (T, bool)> innerFunction)
+		{
+			this.innerFunction = innerFunction;
+		}
+
+		public static CancellableMonad<T> FromValue (T value) => new CancellableMonad<T> (t => (value, false));
+
+		public (T value, bool wasCanceled) Run (CancellationToken? token = null)
+		{
+			return innerFunction (token ?? CancellationToken.None);
+		}
+	}
+
+	public static class Cancellable
+	{
+		public static CancellableMonad<T> MaybeDo<T> (Func<T> innerFunc)
+		{
+			return new CancellableMonad<T> (token => (innerFunc (), false));
+		}
+	}
+
+	public class CancellableExpressionBuilder : IMonadExpressionBuilder
+	{
+		public static readonly CancellableExpressionBuilder Instance = new CancellableExpressionBuilder ();
+
+		public IMonad<T> Bind<U, T> (IMonad<U> m, Func<U, IMonad<T>> f)
+		{
+			var previous = (CancellableMonad<U>)m;
+			return new CancellableMonad<T> (token => {
+				if (token.IsCancellationRequested)
+					return (default (T), true);
+				var (result, wasCanceled) = previous.Run (token);
+				if (wasCanceled)
+					return result is T compatible ? (compatible, true) : (default (T), true);
+				var next = (CancellableMonad<T>)f (result);
+				if (token.IsCancellationRequested)
+					return result is T compatible ? (compatible, true) : (default (T), true);
+				return next.Run (token);
+			});
+		}
+
+		public IMonad<T> Return<T> (T v) => CancellableMonad<T>.FromValue (v);
+
+		public IMonad<T> Zero<T> () => CancellableMonad<T>.FromValue (default (T));
+
+		public IMonad<T> Combine<T> (IMonad<T> m, IMonad<T> n)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
